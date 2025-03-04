2668347ea4dea2a2acb9b58124af270f183c5c3d
Diego <bastosdiegol@gmail.com>
2025-03-04 08:01:35 -0500

    chore: add MapPixel game component

---
+++ components/MapPixel.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { View, Image } from "react-native";
+
+const MapPixel = ({ body, color, width, height }) => {
+  const xPos = body.position.x - width / 2;
+  const yPos = body.position.y - height / 2;
+
+  const [texture, setTexture] = useState(null);
+
+  useEffect(() => {
+    switch (color) {
+      case "rgb(0, 0, 0)":
+        setTexture(require("../assets/cobblestone.png"));
+        break;
+      case "rgb(0, 255, 0)":
+        setTexture(require("../assets/grass_block.png"));
+        break;
+      default:
+        setTexture(null);
+    }
+  }, [color]);
+
+  return (
+    <View
+      style={{
+        position: "absolute",
+        left: xPos,
+        top: yPos,
+        width: width,
+        height: height,
+      }}
+    >
+      {texture && (
+        <Image source={texture} style={{ width: width, height: height }} />
+      )}
+    </View>
+  );
+};
+
+export default MapPixel;
