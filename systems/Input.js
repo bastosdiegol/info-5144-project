78097d6c032416ab84573f60506714195ab9fbd6
Diego <bastosdiegol@gmail.com>
2025-03-04 08:08:37 -0500

    chore: add player velocity based on screen swipes

--- systems/Input.js
+++ systems/Input.js
@@ -1,4 +1,5 @@
 import Matter from "matter-js";
+import { PLAYER_SPEED } from "../utils/constants";
 
 /**
  * Input system.
@@ -7,6 +8,17 @@ import Matter from "matter-js";
  * @returns {Object} Entities object.
  */
 const Input = (entities, { touches }) => {
+  // Apply Velocity on Player Entity Based on Screen Swipes
+  touches
+    .filter((t) => t.type === "move")
+    .forEach((t) => {
+      const { delta } = t;
+      Matter.Body.setVelocity(entities.player.body, {
+        x: delta.pageX * (PLAYER_SPEED / 5),
+        y: delta.pageY * (PLAYER_SPEED / 5),
+      });
+    });
+
   return entities;
 };
 
